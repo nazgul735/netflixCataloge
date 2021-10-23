bfaa1b86642d20545f4731dd685df4cb68e8d278
Aleksander Obrestad <aleksaob@stud.ntnu.no>
2021-10-23 11:46:29 +0000

    Detailedview med router

---
+++ frontend/src/pages/detailedMovie.tsx
@@ -0,0 +1,99 @@
+import { url } from 'inspector';
+import * as React from 'react';
+import { useQuery } from "@apollo/client";
+import { REVIEW, SINGLE_MOVIE } from '../api/graphqlQueries';
+import ReviewCard from '../components/ReviewCard';
+
+
+interface Review {
+    rating: number,
+    review?: string,
+}
+
+interface GetMoviesQueryType {
+    getMovieByID: Movie
+}
+
+interface ReviewProps {
+    movieID: string
+}
+
+
+interface Movie {
+    id: string,
+    title: string,
+    year: string,
+    genres: string[],
+    actors: string[],
+    posterurl: string,
+    storyline: string,
+}
+
+function Review({ movieID }: ReviewProps) {
+
+    const { data, error, loading } = useQuery(REVIEW, { variables: { "movieId": movieID } });
+
+    // console.log(error);
+    // console.log(loading);
+
+    console.log(error?.graphQLErrors[0].message);
+
+
+    if (error?.graphQLErrors[0].message == "Error: Reviews for given movie not found") {
+        return (
+            <div>DUMMYTEXT 😱</div>
+        )
+    }
+    else if (error) {
+        return (
+            <div> Could not connect to reviews😱 </div>
+        )
+    }
+
+    if (loading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+
+    const reviewsToDisplay = data?.getReviewsByMovie && data?.getReviewsByMovie.map((review: Review) => <ReviewCard review={review.review} rating={review.rating} />);
+
+
+
+    return (
+        <div>
+            <p>{reviewsToDisplay}</p>
+        </div>
+    )
+
+}
+
+
+
+function DetailedMovie() {
+
+
+    const URLtoArray = window.location.href.split("/");
+    const movieID = URLtoArray[URLtoArray.length - 1];
+
+
+    console.log(movieID);
+
+
+    const { data } = useQuery<GetMoviesQueryType>(SINGLE_MOVIE, { variables: { "movieId": movieID } })
+
+    console.log(data);
+
+    const movieToDisplay = data?.getMovieByID;
+
+    return (
+        <div>
+            <h1> {movieToDisplay?.title} </h1>
+            <Review movieID={movieID} />
+        </div>
+
+    )
+
+}
+export default DetailedMovie;
+
