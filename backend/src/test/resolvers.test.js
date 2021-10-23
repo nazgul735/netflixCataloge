428fb57158f58208d218bff7f657c704a319589f
Sebasv <sebasv@stud.ntnu.no>
2021-10-23 16:21:50 +0200

    pseudo

---
+++ backend/src/test/resolvers.test.js
@@ -0,0 +1,48 @@
+import resolvers from '../resolvers'
+import request from 'supertest'
+
+describe("POST /users", () => {
+    describe("create review without rating and validate authorization", () => {
+        test("", async () => {
+            
+            })
+            expect().
+          })
+    
+          const registerMutation = `
+mutation Register($data: RegisterInput!) {
+  register(
+    data: $data
+  ) {
+    username,
+    email,
+    password,
+    confirmPassword
+    
+  }
+}
+`;
+
+describe("Register", () => {
+    it("create user", async () => {
+      const user = {
+        usernameName: faker.name.firstName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        confirmPassword: faker.internet.confirmPassword()
+      };
+
+    expect(response).toMatchObject({
+    data: {
+        me: {
+        id: `${user.id}`,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email
+        }
+    }
+    });
+});
+}
+
+
