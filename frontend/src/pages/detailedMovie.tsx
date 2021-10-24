84fc9c44ddb0fd53d6f7c104d9496f099a0a82b6
Victoria <victorah@stud.ntnu.no>
2021-10-24 23:44:14 +0200

    finish createReview functionality

--- frontend/src/pages/detailedMovie.tsx
+++ frontend/src/pages/detailedMovie.tsx
@@ -1,13 +1,17 @@
-import { url } from 'inspector';
-import * as React from 'react';
-import { useQuery } from "@apollo/client";
-import { REVIEW, SINGLE_MOVIE } from '../api/graphqlQueries';
+import { useMutation, useQuery } from "@apollo/client";
+import { CREATE_REVIEW, REVIEW, SINGLE_MOVIE } from '../api/graphqlQueries';
 import ReviewCard from '../components/ReviewCard';
-
-
-interface Review {
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import StarIcon from '@mui/icons-material/Star';
+import TextField from '@mui/material/TextField';
+import { FunctionComponent, useState } from "react";
+import Button from '@mui/material/Button';
+import { ErrorMessage } from "./register/Register";
+interface ReviewType {
     rating: number,
     review?: string,
+    username: string
 }
 
 interface GetMoviesQueryType {
@@ -28,6 +32,78 @@ interface Movie {
     posterurl: string,
     storyline: string,
 }
+interface CreateReviewProps {
+    movieID: string
+}
+
+
+const CreateReview: FunctionComponent<CreateReviewProps> = ({movieID}) => {
+    const [rating, setRating] = useState<number>(3);
+    const [review, setReview] = useState<string>(""); 
+    const [publishReview] = useMutation(CREATE_REVIEW);
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [showCreatedReview, setShowCreatedReview] = useState<boolean>(false);
+    const [data, setData] = useState<ReviewType>({rating:1, review:"", username:""});
+    console.log(review)
+    const labels: { [index: string]: string } = {
+        1: 'Very bad',
+        2: 'Poor',
+        3: 'Ok',
+        4: 'Good',
+        5: 'Excellent',
+      };
+    const handlePublishReview = async ()=>{
+        try {
+            // Try create review, will work if user is logged in
+            const { data } = await publishReview({ variables: {
+                "rating": rating as number,
+                "movieId": movieID,
+                "review": review,
+              } });
+              setData(data.createReview);
+              setShowCreatedReview(true); 
+     
+    }// If error, then invalid token is given or invalid argument. Set error message if error given
+     catch (error:any) {
+        setErrorMessage("Something went wrong");
+      }
+}
+    const displayCreatedReview = showCreatedReview &&  <ReviewCard review={data.review} rating={data.rating} username={data.username}/> 
+    return (
+        <div>
+        <ErrorMessage message={errorMessage}/>
+        <Box sx={{
+        width: 300,
+        display: 'flex',
+        alignItems: 'center',
+        margin: "0 40vw"
+      }} >
+        <Rating
+            name="rating"
+            value={rating}
+            precision={1}
+            onChange={(e:any)=>setRating(parseInt(e.target.value))}
+            emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+        />
+        <Box sx={{ ml: 2 }}>{labels[rating]}</Box>
+        </Box>
+        <div style={{display:"flex", alignItems:"center", width:"50vw", flexDirection:'column', 
+        justifyContent:"space-between", margin:"0 auto"}}>
+        <TextField
+            id="review"
+            label="Review"
+            placeholder="Write your review here"
+            multiline
+            onChange={(event: React.ChangeEvent<HTMLInputElement>)=>{setReview(event.target.value as string)}}
+        />
+        <Button onClick={()=> handlePublishReview()} variant="outlined">Publish review</Button>
+        </div>
+        {displayCreatedReview}
+        </div>
+        );
+}
+ 
+
 
 function Review({ movieID }: ReviewProps) {
 
@@ -51,14 +127,14 @@ function Review({ movieID }: ReviewProps) {
             <div>Loading...</div>
         )
     }
-
-    const reviewsToDisplay = data?.getReviewsByMovie && data?.getReviewsByMovie.map((review: Review) => <ReviewCard review={review.review} rating={review.rating} />);
+    console.log(data?.getReviewsByMovie)
+    const reviewsToDisplay = data?.getReviewsByMovie && data?.getReviewsByMovie.map((review: ReviewType) => <ReviewCard review={review.review} rating={review.rating} username={review.username} />);
 
 
 
     return (
         <div>
-            <p>{reviewsToDisplay}</p>
+            {reviewsToDisplay}
         </div>
     )
 
@@ -81,10 +157,10 @@ function DetailedMovie() {
  
 
     const movieToDisplay = data?.getMovieByID;
-
     return (
         <div>
             <h1> {movieToDisplay?.title} </h1>
+            {sessionStorage.getItem("jwt") &&<CreateReview movieID={movieID}/>}
             <Review movieID={movieID} />
         </div>
 
